7ec19844133133dac0a1db6f124fbdf8ddaaf0ee
Michel Cordova <cordovamichel540@gmail.com>
2024-06-30 16:37:21 -0600

    third commit

---
+++ src/controllers/cart.controllers.js
@@ -0,0 +1,97 @@
+const catchError = require('../utils/catchError');
+const Cart = require('../models/Cart');
+const Product = require('../models/Product');
+const Category = require('../models/Category');
+
+const getAll = catchError(async (req, res) => {
+  const userId = req.user.id
+  const results = await Cart.findAll({
+    where: { userId },
+    include: [
+      {
+        model: Product,
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+        // attributes: ['title']
+        include: [
+          {
+            model: Category,
+            attributes: ['name']
+          }
+        ]
+      }
+    ]
+  });
+  return res.json(results);
+});
+
+const create = catchError(async (req, res) => {
+  const { quantity, productId } = req.body
+  const userId = req.user.id
+
+  // const product = await Product.findByPk(productId)
+  // if (!product) return res.sendStatus(404)
+
+  const body = { userId, quantity, productId }
+
+  const result = await Cart.create(body);
+
+  return res.status(201).json(result);
+});
+
+const getOne = catchError(async (req, res) => {
+  const { id } = req.params;
+  const userId = req.user.id
+  const result = await Cart.findByPk(id, {
+    where: { userId },
+    include: [
+      {
+        model: Product,
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+        // attributes: ['title']
+        include: [
+          {
+            model: Category,
+            attributes: ['name']
+          }
+        ]
+      }
+    ]
+  });
+  if (!result) return res.sendStatus(404);
+  return res.json(result);
+});
+
+const remove = catchError(async (req, res) => {
+  //quantity, productId, userId, id, times
+
+  const { id } = req.params;
+  const result = await Cart.destroy({
+    where: {
+      id,
+      userId: req.user.id
+    }
+  });
+  if (!result) return res.sendStatus(404);
+  return res.sendStatus(204);
+});
+
+const update = catchError(async (req, res) => {
+  const userId = req.user.id
+  //quantity, productId, userId, id, times
+  const { id } = req.params;
+  const { quantity } = req.body
+  const result = await Cart.update(
+    { quantity }, //quantity, productId, userId
+    { where: { id, userId }, returning: true }
+  );
+  if (result[0] === 0) return res.sendStatus(404);
+  return res.json(result[1][0]);
+});
+
+module.exports = {
+  getAll,
+  create,
+  getOne,
+  remove,
+  update
+}
--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -3,6 +3,7 @@ const app = require('../app')
 
 const BASE_URL = '/api/v1/users'
 let TOKEN
+let userId
 
 beforeAll(async () => {
     const body = {
@@ -33,6 +34,8 @@ test("POST -> 'BASE_URL', should return statusCode 201, and res.body.firstName =
      .post(BASE_URL)
      .send(user)
 
+     userId = res.body.id
+
 
     expect(res.statusCode).toBe(201)
     expect(res.body).toBeDefined()
@@ -46,4 +49,65 @@ test("GET -> 'BASE_URL', should return statusCode 200, and res.body.length === 2
     // console.log(res.body);s
     expect(res.status).toBe(200)
   
+  })
+
+test("PUT -> 'URL_BASE/:id', should return status code 200 and res.body.firstName === user.firstName", async () => {
+    const user = {
+      firstName: "Alejandro",
+    }
+  
+    const res = await request(app)
+      .put(`${BASE_URL}/${userId}`)
+      .send(user)
+      .set('Authorization', `Bearer ${TOKEN}`)
+  
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.firstName).toBe(user.firstName)
+  
+  })
+
+test("POST -> 'BASE_URL/login', should return statusCode 401", async () => {
+
+    const body = {
+      email: "mcordova@gmail.com",
+      password: "invalid access"
+    }
+  
+    const res = await request(app)
+      .post(`${BASE_URL}/login`)
+      .send(body)
+  
+  
+  
+    expect(res.statusCode).toBe(401)
+  })
+
+test("POST -> 'BASE_URL/login', should return statusCode 200, res.body.user and res.body.token to be defined, and res.body.user.email === body.email", async () => {
+    const body = {
+      email: "sperez@mail.com",
+      password: "samantha1234"
+    }
+  
+    const res = await request(app)
+      .post(`${BASE_URL}/login`)
+      .send(body)
+  
+    // console.log(res.body);
+  
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.user).toBeDefined()
+    expect(res.body.token).toBeDefined()
+    expect(res.body.user.email).toBe(body.email)
+  })
+
+
+
+test("DELETE -> 'URL_BASE/:id', should return status code 204", async () => {
+    const res = await request(app)
+      .delete(`${BASE_URL}/${userId}`)
+      .set("Authorization", `Bearer ${TOKEN}`)
+  
+    expect(res.status).toBe(204)
   })
