b13d9508add8183547813dd6f2524cf7cd9b0c07
Michel Cordova <cordovamichel540@gmail.com>
2024-07-03 23:39:00 -0600

    Seventh commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const router = require('./routes');
 const errorHandler = require('./utils/errorHandler');
 require('dotenv').config();
+const path = require("path");
 
 // Esta es nuestra aplicación
 const app = express();
